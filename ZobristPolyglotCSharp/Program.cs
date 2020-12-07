431809c6c72eb1d130a91ea261a8de554e067310
Chay Palton <chayp@hotmail.com>
2020-12-07 02:51:56 +0000

    Update

--- ZobristPolyglotCSharp/Program.cs
+++ ZobristPolyglotCSharp/Program.cs
@@ -10,7 +10,25 @@ namespace ZobristPolyglotCSharp
         
         static void Main(string[] args)
         {
-         
+            Console.WriteLine("Chay's chess.\n");
+
+            string filename = "..\\..\\..\\..\\..\\book\\komodo.bin";
+            string bookname = "komodo";
+
+            System.Console.WriteLine("Try load openbook {0}", filename);
+            OpeningBooks.Book bookCodeKomodo = OpeningBooks.LoadBook(bookname, filename);
+
+
+            if (bookCodeKomodo != null)
+            {
+                System.Console.WriteLine("Book {0} opened", filename);
+            }
+            else
+            {
+                System.Console.WriteLine("Could load openbook {0}", filename);
+            }
+
+
         }
     }
 }
